0b1d0d68f974d1639439c18cfccd735b9d91ab07
Patrick Lafrance <plafrance@crosemont.qc.ca>
2017-08-10 14:35:20 -0400

    Ajout la possibilité de réinitialiser un conteneur. Enlevé l'anti-bruteforce et remplacé par la limitation d'écouter les connexions uniquement à partir de l'hôte local.

--- API/app.js
+++ API/app.js
@@ -13,13 +13,13 @@ var app = express.createServer();
 var port=12380;
 
 
-var ExpressBrute = require('express-brute');
-var store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
-var bruteforce = new ExpressBrute(store,{
-    freeRetries: 50,
-    lifetime: 3600
-});
-
+//var ExpressBrute = require('express-brute');
+//var store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+//var bruteforce = new ExpressBrute(store,{
+//    freeRetries: 2,
+//    minWait: 500
+//});
+//
 app.use(express.static(__dirname));
 app.use(express.bodyParser());
 
@@ -38,7 +38,7 @@ function random(size) {
 }
 
 
-app.post('/compile',bruteforce.prevent,function(req, res) 
+app.post('/compile',function(req, res) 
 {
 
     var language = req.body.language;
@@ -48,8 +48,15 @@ app.post('/compile',bruteforce.prevent,function(req, res)
 
     var folder= 'temp/' + random(10); //folder in which the temporary folder will be saved
     var path=__dirname+"/"; //current working path
-    var vm_name='remotecompiler'; //name of virtual machine that we want to execute
-    var timeout_value=20;//Timeout Value, In Seconds
+
+    if(arr.compilerArray[language][0]=="sshd"){
+	var vm_name=req.body.vm_name; //name of virtual machine that we want to execute
+	var timeout_value=600;//Timeout Value, In Seconds
+    }
+    else{
+	var vm_name='remotecompiler'; //name of virtual machine that we want to execute
+	var timeout_value=20;//Timeout Value, In Seconds
+    }
 
     //details of this are present in DockerSandbox.js
     var sandboxType = new sandBox(timeout_value,path,folder,parameters,vm_name,arr.compilerArray[language][0],arr.compilerArray[language][1],code,arr.compilerArray[language][2],arr.compilerArray[language][3],arr.compilerArray[language][4],stdin);
@@ -57,19 +64,23 @@ app.post('/compile',bruteforce.prevent,function(req, res)
 
     //data will contain the output of the compiled/interpreted code
     //the result maybe normal program output, list of error messages or a Timeout error
+    if(arr.compilerArray[language][0]=="sshd"){
+	sandboxType.run(function(contid, ip, port, res_valid)
+			{
+    			    res.send({cont_id:contid, add_ip:ip, add_port:port, resultat:res_valid});
+			});
+    }
+    else{
     sandboxType.run(function(data,exec_time,err)
     {
         //console.log("Data: received: "+ data)
     	res.send({output:data, langid: language,code:code, errors:err, time:exec_time});
     });
+    }
    
 });
 
-
-app.get('/', function(req, res) 
-{
-    res.sendfile("./index.html");
-});
-
 console.log("Listening at "+port)
-app.listen(port);
+
+//N'accepte que les connexions locales
+app.listen(port, '127.0.0.1');
